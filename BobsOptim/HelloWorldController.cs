455191efaa157af5e64e981ef67b89091cbe0fca
Philippe Edwards <philippe.e16@hotmail.fr>
2022-04-09 01:12:19 +0200

    added hello world window

---
+++ BobsOptim/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿using Hearthstone_Deck_Tracker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobsOptim
+{
+    public class HelloWorldController
+    {
+        private HelloWorldWindow helloWorldWindow;
+
+        public HelloWorldController(HelloWorldWindow helloWorldWindow)
+        {
+            this.helloWorldWindow = helloWorldWindow;
+        }
+
+        internal void GameStart()
+        {
+            helloWorldWindow.Update("Game Start");
+        }
+
+        internal void InMenu()
+        {
+            helloWorldWindow.Update("In Menu");
+        }
+
+        internal void TurnStart(ActivePlayer player)
+        {
+            switch (player)
+            {
+                case ActivePlayer.Opponent:
+                    helloWorldWindow.Update("opponent");
+                    break;
+                case ActivePlayer.Player:
+                    helloWorldWindow.Update("player");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ BobsOptim/HelloWorldWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BobsOptim
+{
+    public partial class HelloWorldWindow : UserControl
+    {
+        public HelloWorldWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Update(String s)
+        {
+            Log.Info("updated hello world");
+            hwLabel.Content = s;
+            UpdatePosition();
+            Show();
+        }
+
+        public void UpdatePosition()
+        {
+            Canvas.SetTop(this, Core.OverlayWindow.Height * 5 / 100);
+            Canvas.SetRight(this, Core.OverlayWindow.Width * 20 / 100);
+        }
+        public void Show()
+        {
+            this.Visibility = Visibility.Visible;
+        }
+
+        public void Hide()
+        {
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
